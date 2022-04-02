bde56d48bd2123cbce175d9649a5f1bc27063f86
alemat13 <alemat13@gmail.com>
2022-04-02 16:22:12 +0200

    Initial commit

---
+++ src/main/java/fr/olivex/invader/InvaderApplication.java
@@ -0,0 +1,13 @@
+package fr.olivex.invader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/olivex/invader/InvaderApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.olivex.invader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
